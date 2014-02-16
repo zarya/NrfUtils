128a2f7ac5db6a2996ef536d0296fa6e69f296d8
Rudy Hardeman <zarya@gigafreak.net>
2014-02-16 10:06:02 +0100

    Minor changes to ping

--- ping.py
+++ ping.py
@@ -3,6 +3,7 @@ from struct import *
 import sys
 import socket
 import time
+import random
 from optparse import OptionParser
 
 current_milli_time = lambda: int(round(time.time() * 1000))
@@ -11,9 +12,9 @@ if sys.argv[1]is None:
     print "run like `ping.py 5`"
     sys.exit(1)
 
-
-current_time = int(str(current_milli_time())[-4:])
-data = pack('<HcH', int(sys.argv[1]), 'P', current_time)
+ping_id = random.randrange(0, 9999)
+current_time = current_milli_time()
+data = pack('<HcH', int(sys.argv[1]), 'P', ping_id)
 s = socket.socket()         # Create a socket object
 host = "10.38.18.105" # Get local machine name
 port = 12345                # Reserve a port for your service.
@@ -21,10 +22,14 @@ s.connect((host, port))
 s.send(data)
 while 1:
     data = s.recv(64)
+    now_time = current_milli_time()
     if data[:1] == "Q":
         reply = data.split()
-        current_time = int(str(current_milli_time())[-4:])
-        reply_time = float(current_time - int(reply[2]))
-        print "Reply from node: %s ttl: %0.02f ms." % (reply[1],reply_time)
+        if int(reply[2]) == ping_id:
+            reply_time = float(now_time - current_time)
+            print "Reply from node: %s ttl: %0.02f ms." % (reply[1],reply_time)
+            break
+    if (now_time - current_time) > 9000:
+        print "Ping timeout"
         break 
 s.close
